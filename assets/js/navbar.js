8c6d6318a94ebac4a6f71e75e49cd75748488130
Erne <edainesi@fi.uba.ar>
2021-09-13 20:46:13 -0300

    refactor code

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,16 +1,29 @@
 const navBar = document.querySelector("#navbarNavAltMarkup");
 
+const closeMobileNavbar = () => {
+	const offCanvasBackdrop = document.querySelector(".offcanvas-backdrop");
+	const body = document.querySelector("body");
+	body.removeAttribute("style");
+	navBar.style.visibility = "hidden";
+	navBar.setAttribute("aria-hidden", true);
+	navBar.removeAttribute("aria-modal");
+	navBar.removeAttribute("role");
+	offCanvasBackdrop.remove();
+	navBar.classList.remove("show");
+};
+
+const changeSelectedNavLink = navLink => {
+	const prevSelectedNavLink = document.querySelector(".selected");
+	prevSelectedNavLink.classList.remove("selected");
+	navLink.classList.add("selected");
+};
+
 navBar.addEventListener("click", e => {
-	if (e.target.classList.contains("nav-link") && navBar.classList.contains("show") && screen.width <= 992) {
-		const offCanvasBackdrop = document.querySelector(".offcanvas-backdrop");
-		const body = document.querySelector("body");
-		body.removeAttribute("style");
-		navBar.style.visibility = "hidden";
-		navBar.setAttribute("aria-hidden", true);
-		navBar.removeAttribute("aria-modal");
-		navBar.removeAttribute("role");
-		offCanvasBackdrop.remove();
-		navBar.classList.remove("show");
+	const target = e.target;
+	if (target.classList.contains("nav-link") && navBar.classList.contains("show") && screen.width <= 992) {
+		closeMobileNavbar();
+	} else if (screen.width > 992 && !target.classList.contains("selected")) {
+		changeSelectedNavLink(target);
 	}
 });
 
